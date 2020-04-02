ba250412ab294d43459d6f808a28dafe0cc65161
GatGIS <58220276+GatGIS@users.noreply.github.com>
2020-04-02 11:24:58 +0300

    testin

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+//// global variable
+//var data;
+//
+//$.get('books.json', function(d) {
+//  //data = JSON.parse(d);
+//  // loop through all books
+//  d.books.forEach(function(b) {
+//    // now you can put every book in your <div>
+//    $("#books").append(`<div><h2>${b.title}</h2><p>${b.author}</p></div>`);
+//  });
+//});
+
 // global variable
 var data;
 
@@ -6,6 +18,18 @@ $.get('books.json', function(d) {
   // loop through all books
   d.books.forEach(function(b) {
     // now you can put every book in your <div>
-    $("#books").append(`<div><h2>${b.title}</h2><p>${b.author}</p></div>`);
+    $("#books").append(`<figure class="card">
+    <div class="card__hero">
+        <img src="data/img/macbook-pro-15.jpg" alt="" class="card__img">
+    </div>
+    <h2 class="card__name">${b.title}</h2>
+    <p class="card__detail"><span class="emoji-left">🖥</span> 15-inch Retina display</p>
+    <p class="card__detail"><span class="emoji-left">🧮</span> 6-core Intel i7, 8th generation</p>
+    <div class="card__footer">
+        <p class="card__price">$3199</p>
+        <a href="laptop.html" class="card__link">Check it out <span class="emoji-right">👉</span></a>
+    </div>
+    </figure>`);
   });
 });
+
