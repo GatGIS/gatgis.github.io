887828e9842363ab76d7b34311af7a3c5352b58a
GatGIS <58220276+GatGIS@users.noreply.github.com>
2020-04-02 13:10:48 +0300

    new list file

--- script.js
+++ script.js
@@ -13,23 +13,50 @@
 // global variable
 var data;
 
-$.get('books.json', function(d) {
+$.get('sample.json', function(d) {
   //data = JSON.parse(d);
   // loop through all books
-  d.books.forEach(function(b) {
+  d.forEach(function(b) {
     // now you can put every book in your <div>
-    $("#books").append(`<figure class="card">
+    $("#kartinas").append(`<figure class="card">
     <div class="card__hero">
-        <img src="data/img/macbook-pro-15.jpg" alt="" class="card__img">
+        <img src="https://www.maxima.lv${b.Bilde}" alt="" class="card__img">
     </div>
-    <h2 class="card__name">${b.title}</h2>
-    <p class="card__detail"><span class="emoji-left">🖥</span> 15-inch Retina display</p>
-    <p class="card__detail"><span class="emoji-left">🧮</span> 6-core Intel i7, 8th generation</p>
+    <h2 class="card__name">${b.Prece}</h2>
+    <p class="card__detail">${b.Atlaide}</p>
+    <p class="card__detail">${b.Cenalitra}</p>
     <div class="card__footer">
-        <p class="card__price">$3199</p>
-        <a href="laptop.html" class="card__link">Check it out <span class="emoji-right">👉</span></a>
+        <p class="card__price">${b.Euro}.${b.Centi} Eur</p>
     </div>
     </figure>`);
   });
 });
 
+//
+//$(function() {                                                                                                                                                                                                                                 
+//  function createStatusCard(name,callStatus,handlingTime) {                                                                                                                                                                                    
+//    var status_class_map = {                                                                                                                                                                                                                   
+//      "On Call" : "active",                                                                                                                                                                                                                    
+//      "Idle" : "idle"                                                                                                                                                                                                                          
+//    };                                                                                                                                                                                                                                         
+//    var $content = $("<div/>").addClass("status-card").addClass(function(){                                                                                                                                                                    
+//      return status_class_map[callStatus] || "away";                                                                                                                                                                                           
+//    });                                                                                                                                                                                                                                        
+//    $content.html('<div class="agent-details">' +                                                                                                                                                                                              
+//      '<span class="agent-name">' + name + '</span>' +                                                                                                                                                                                         
+//      '<span class="handling-state">' + callStatus + '</span>' +                                                                                                                                                                               
+//      '<span class="handling-time">' + handlingTime + '</span>' +                                                                                                                                                                              
+//      '</div>' +                                                                                                                                                                                                                               
+//      '<div class="status-indicator"></div>');                                                                                                                                                                                                 
+//    return $content;                                                                                                                                                                                                                           
+//  }                                                                                                                                                                                                                                            
+//  $.getJSON('agents.json', function(a) {                                                                                                                                                                                                       
+//    $.each(a.agents, function(b, c) {                                                                                                                                                                                                          
+//      $("#left").append(createStatusCard(c.name,c.callStatus,c.handlingTime));                                                                                                                                                                 
+//    });                                                                                                                                                                                                                                        
+//  });                                                                                                                                                                                                                                          
+//}); 
+//
+//var d1 = document.getElementById('one');
+//d1.insertAdjacentHTML('beforeend', '<div id="two">two</div>');
+//
