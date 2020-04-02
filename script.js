6a24bf39ecf58cadfab20a611c4364c0ebaafe93
GatGIS <58220276+GatGIS@users.noreply.github.com>
2020-04-02 10:09:21 +0300

    add shit

---
+++ script.js
@@ -0,0 +1,11 @@
+// global variable
+var data;
+
+$.get('books.json', function(d) {
+  data = JSON.parse(d);
+  // loop through all books
+  data.books.forEach(function(b) {
+    // now you can put every book in your <div>
+    $("#books").append(`<div><h2>${b.title}</h2><p>${b.author}</p></div>`);
+  });
+});
