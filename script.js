14eb4880cefe0dc03e4c2593d1b3bcf14274df77
GatGIS <58220276+GatGIS@users.noreply.github.com>
2020-04-07 00:58:30 +0300

    big reworkz

--- script.js
+++ script.js
@@ -1,37 +1,55 @@
-//// global variable
-//var data;
-//
-//$.get('books.json', function(d) {
-//  //data = JSON.parse(d);
-//  // loop through all books
-//  d.books.forEach(function(b) {
-//    // now you can put every book in your <div>
-//    $("#books").append(`<div><h2>${b.title}</h2><p>${b.author}</p></div>`);
-//  });
-//});
-
 // global variable
 var data;
 
 $.get('sample.json', function(d) {
-  //data = JSON.parse(d);
-  // loop through all books
-  d.forEach(function(b) {
-    // now you can put every book in your <div>
-    $("#kartinas").append(`<figure class="card">
-    <div class="card__hero">
-        <img src="https://www.maxima.lv${b.Bilde}" alt="" class="card__img">
-    </div>
-    <h2 class="card__name">${b.Prece}</h2>
-    <p class="card__detail">${b.Atlaide}</p>
-    <p class="card__detail">${b.Cenalitra}</p>
-    <div class="card__footer">
-        <p class="card__price">${b.Euro}.${b.Centi} Eur</p>
-    </div>
-    </figure>`);
-  });
+	d.forEach(function(b) {
+		$("#kartinas").append(`<figure class="card">
+		<div class="card__hero">
+			<img src="https://www.maxima.lv${b.Bilde}" alt="" class="card__img">
+			<div class="discount-icon">
+				<span class="discount-icon" style="background-image: url('https://www.maxima.lv/uploads/sale/empty-4613.png')">${b.Atlaide}</span>
+			</div>
+			<div class="price-icon">
+				<span class="block" style="price-icon">${b.Euro}.${b.Centi} €</span>
+			</div>
+			<div class="volume-price">
+				<span class="volume-price">${b.Cenalitra}</span>
+			</div>
+		</div>
+		<h2 class="card__name">${b.Prece}</h2>
+		</figure>`);
+	});
 });
+/*
+$.get('sample.json', function(d) {
+	var $card, $hero, $name, $discount, $volumePrice, $footer;
+	$card = $('<figure class="card"></figure>');
+	$hero = $('<div class="cardhero"><img src="https://www.maxima.lv/$%7Bb.Bilde%7D" alt="" class="cardimg"></div>');
+	$name = $('<h2 class="cardname">${b.Prece}</h2>');
+	$discount = $('<p class="carddetail">${b.Atlaide}</p>');
+	$volumePrice = $('<p class="carddetail">${b.Cenalitra}</p>');
+	$footer = $('<div class="cardfooter"><p class="card__price">${b.Euro}.${b.Centi} Eur</p></div>');
 
+	$card
+		.append($hero)
+		.append($name);
+
+	if (...) {
+		$card
+			.append($discount);
+	}
+
+	$card
+		.append($volumePrice)
+		.append($footer);
+
+	d.forEach(function(b) {
+		// now you can put every book in your <div>
+		$("#kartinas").append($card);
+	});
+
+});
+*/
 //
 //$(function() {                                                                                                                                                                                                                                 
 //  function createStatusCard(name,callStatus,handlingTime) {                                                                                                                                                                                    
