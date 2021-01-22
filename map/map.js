aca22ff50bc0327a20ca88b4d9a54420f63e80da
GatGIS <58220276+GatGIS@users.noreply.github.com>
2021-01-22 15:49:03 +0200

    working map added

--- map/map.js
+++ map/map.js
@@ -1,16 +1,16 @@
-window.onload = init;
+window.onload = init();
 function init(){
     const map = new ol.Map({
         view: new ol.View({
-            center: [0, 0],
-            zoom: 2
+            center: [2682857.1135981246, 7748815.42281414],
+            zoom: 12
         }),
         layers: [
             new ol.layer.Tile({
                 source: new ol.source.OSM()
             })
         ],
-        target: 'js-map'
+        target: 'map'
     })
     map.on('click', function(e){
         console.log(e.coordinate);
